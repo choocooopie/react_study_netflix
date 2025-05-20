56fb38943295e2c970ba6d2d6068e96046402808
Alex Oh <odange2119@gmail.com>
2025-05-20 13:30:15 +0900

    first commit55

--- src/pages/Homepage/components/MovieCard/MovieCard.jsx
+++ src/pages/Homepage/components/MovieCard/MovieCard.jsx
@@ -1,7 +1,6 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Badge from "react-bootstrap/Badge";
-import { AiFillStar } from "react-icons/ai";
-import { BsPeopleFill } from "react-icons/bs";
 import "./MovieCard.style.css";
 
 const GENRE = { 28: "Action", 35: "Comedy", 12: "Adventure" };
@@ -10,7 +9,9 @@ const MovieCard = ({ movie }) => (
   <div
     className="movie-card"
     style={{
-      backgroundImage: `url(https://www.themoviedb.org/t/p/w600_and_h900_bestv2${movie.poster_path})`,
+      backgroundImage: movie.poster_path
+        ? `url(https://www.themoviedb.org/t/p/w600_and_h900_bestv2${movie.poster_path})`
+        : "none",
     }}
   >
     <div className="overlay">
@@ -18,16 +19,17 @@ const MovieCard = ({ movie }) => (
         <h5 className="movie-title">{movie.title}</h5>
         <hr />
         <div className="badge-wrap">
-          {movie.genre_ids.slice(0, 3).map((id) => (
-            <Badge bg="danger" key={id}>
-              {GENRE[id] ?? id}
-            </Badge>
-          ))}
-       </div>
-     </div>
-   
+          {Array.isArray(movie.genre_ids) &&
+            movie.genre_ids.slice(0, 3).map((id) => (
+              <Badge bg="danger" key={id}>
+                {GENRE[id] ?? id}
+              </Badge>
+            ))}
+        </div>
+      </div>
+
       <div className="overlay-footer">
-        <span>평점 {movie.vote_average.toFixed(1)}</span>
+        <span>평점 {movie.vote_average?.toFixed(1)}</span>
         <span>관객수 {Math.round(movie.popularity)}</span>
         <span className={movie.adult ? "adult" : "under18"}>
           {movie.adult ? "18세 이상" : "18세 미만"}
